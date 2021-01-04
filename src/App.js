154c158d0ef38337cdd86817697e196eb4cba2ee
DRKSLV <56208328+DRKSLV@users.noreply.github.com>
2021-01-04 18:35:29 +0100

    Modular sass

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import './App.css';
+import s from '.style/App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className={s.App}>
+      <header className={s.AppHeader}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
